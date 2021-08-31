f33fc07803b4bdb8f2a30475aa04b375098ac114
leoopez <leoocode@gmail.com>
2021-08-31 16:35:56 -0500

    css

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 /** @format */
+
+//Elemets
+const presentation = document.getElementById('presentation');
+const tecnologies = document.getElementById('tecnologies');
+const projects = document.getElementById('projects');
+const contact = document.getElementById('contact');
+
+const sections = [presentation, tecnologies, projects, contact];
+
+const State = {
+  currentSection: 0,
+};
+
+const moveSection = (direction = "up") => {
+  if (direction) {
+    if (State.currentSection === sections.length - 1) State.currentSection = 0;
+    else State.currentSection++;
+  }
+  else {
+    if (State.currentSection === 0) State.currentSection = sections.length - 1;
+    else State.currentSection--;
+  }
+};
+
+window.addEventListener('keypress', e => {
+    if (e.key === 'Enter') {
+        moveSection("up");
+        sections[State.currentSection].scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+})
